4f6a326852a5ff6bd3090bca03e0cd80efdcd821
samuelescavello <scavellosamuele23@gmail.com>
2024-03-22 15:45:33 +0100

    snack 2

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-
+// ***********************************************SNACK 1*************************************************************COMPLEATED****************
 
 let arrInvitati = [ 
     'Brad Pitt', 
@@ -23,3 +23,53 @@ const listTipografy = arrInvitati.map((el , i)=>{
     return finaList
 })
 console.log(listTipografy)
+
+// ***********************************************SNACK 1*************************************************************COMPLEATED****************
+
+let aula = [
+    {
+        Id: '213',
+        Name: 'Marco della Rovere',
+        Grades: '78',
+    },
+    {
+        Id: '110',
+        Name: 'Paola Cortellessa',
+        Grades: '96',
+    },
+    {
+        Id: '250',
+        Name: 'Andrea Mantegna',
+        Grades: '48',
+    },
+    {
+        Id: '145',
+        Name: 'Gaia Borromini',
+        Grades: '74',
+    },
+    {
+        Id: '196',
+        Name: 'Luigi Grimaldello',
+        Grades: '68',
+    },
+    {
+        Id: '102',
+        Name: 'Piero della Francesca',
+        Grades: '50',
+    },
+    {
+        Id: '120',
+        Name: 'Francesca da Polenta',
+        Grades: '84',
+    },
+]
+
+let listName = aula.map(el => el.Name.toUpperCase());
+console.log(listName)
+
+let voti = aula.filter(el => el.Grades > 70);
+console.log(voti)
+
+let votiEid = aula.filter(el => el.Grades > 70 && el.Id > 120);
+console.log(votiEid)
+
